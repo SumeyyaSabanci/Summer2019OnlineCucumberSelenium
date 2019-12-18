5cfebc1f462babd608a029219b4ea885cde0befc
SumeyyaSabanci <ssabanci@gmail.com>
2019-12-18 15:29:27 -0600

    new commit added

--- src/test/java/com/vytrack/step_definitions/Hook.java
+++ src/test/java/com/vytrack/step_definitions/Hook.java
@@ -1,6 +1,7 @@
 package com.vytrack.step_definitions;
 
 
+import com.vytrack.utilities.Driver;
 import io.cucumber.core.api.Scenario;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
@@ -9,19 +10,22 @@ public class Hook {
 
     @Before
     public void setup(){
-        System.out.println("#############################");
+        System.out.println("##############################");
         System.out.println("Test setup!");
-
+        Driver.get().manage().window().maximize();
     }
 
     @After
     public void teardown(Scenario scenario){
+        //if test failed - do this
         if(scenario.isFailed()){
             System.out.println("Test failed!");
-        }else {
+        }else{
             System.out.println("Cleanup!");
             System.out.println("Test completed!");
         }
-        System.out.println("#############################");
+        System.out.println("##############################");
+        //after every test, we gonna close browser
+        Driver.close();
     }
 }
